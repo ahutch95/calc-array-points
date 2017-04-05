183707332e5af6e44fa66f31198aea59c36cef8d
Austin Hutchinson <Ahutch95@gmail.com>
2017-04-05 14:32:41 -0700

    Initial Creation

---
+++ CalcArrayPoints/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CalcArrayPoints
+//
+//  Created by iGuest on 4/5/17.
+//  Copyright © 2017 iGuest. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
