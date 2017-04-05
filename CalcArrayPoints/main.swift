91a25457158cc8963bc9d8f1254d0916556f013c
Austin Hutchinson <Ahutch95@gmail.com>
2017-04-05 15:36:19 -0700

    Added Array fun:

--- CalcArrayPoints/main.swift
+++ CalcArrayPoints/main.swift
@@ -25,3 +25,42 @@ func divide(a: Int, b: Int) -> Int {
 func generic(type: (Int, Int) -> Int, a: Int, b: Int) -> Int{
     return type(a,b)
 }
+
+func newAdd(a: [Int]) -> Int {
+/*    var sum = 0;
+    for i in 0...a.count-1{
+        sum += a[i]
+    }*/
+    return a.reduce(0, +)
+}
+
+func newMultiply(a: [Int]) -> Int {
+/*    var product = 0;
+    for i in 0...a.count-1{
+        product *= a[i]
+    }*/
+    return a.reduce(1, *)
+}
+
+func count(a: [Int]) -> Int {
+    return a.count
+}
+
+func avg(a: [Int]) -> Int {
+/*    var sum = 0.0;
+    for i in 1...a.count{
+        sum += Double(a[i])
+    }*/
+    return newAdd(a: a) / count(a: a)
+}
+
+func newGeneric(type: ([Int]) -> Int, a: [Int]) -> Int{
+    return type(a) 
+}
+
+print(newGeneric(type: avg ,a: [2,4,6]))
+
+
+
+
+
